59530229a78a38f3a08b27feec179f0cbd8eb4d8
Dmytro Chaban <dmi3coder@gmail.com>
2020-06-09 20:47:47 +0000

    Helpful scripts to build c/cpp project

--- buildc.sh
+++ buildc.sh
@@ -1,3 +1,5 @@
 export LLVM_TOOLCHAIN=$(lli --print-toolchain-path)
-$LLVM_TOOLCHAIN/clang -shared ./src/main/cpp/hello.c -lpolyglot-mock -o hello
+#$LLVM_TOOLCHAIN/clang -shared ./src/main/cpp/hello.c -lpolyglot-mock -o hello
+$LLVM_TOOLCHAIN/gcc -I/usr/local/include -L/usr/local/lib ./src/main/cpp/hello.c -ltensorflow -o hello
+
 mv hello ./src/main/resources/
--- src/main/cpp/hello.c
+++ src/main/cpp/hello.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
-#include <polyglot.h>
+#include <tensorflow/c/c_api.h>
 
 int main() {
-    void *arrayType = polyglot_java_type("int[]");
-    void *array = polyglot_new_instance(arrayType, 4);
-    polyglot_set_array_element(array, 2, 24);
-    int element = polyglot_as_i32(polyglot_get_array_element(array, 2));
-    printf("%d\n", element);
-    return element;
+  printf("Hello from TensorFlow C library version %s\n", TF_Version());
+  return 0;
 }
