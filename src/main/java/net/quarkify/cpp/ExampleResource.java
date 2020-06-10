15c3592dac36b7f5b2ea3983cbac05e0648166fc
dmi3coder <dmi3coder@gmail.com>
2020-06-10 22:34:36 +0300

    Improved ExampleResource to initialize asynchronously

--- src/main/java/net/quarkify/cpp/ExampleResource.java
+++ src/main/java/net/quarkify/cpp/ExampleResource.java
@@ -1,9 +1,11 @@
 package net.quarkify.cpp;
 
+import io.quarkus.runtime.StartupEvent;
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Source;
 import org.graalvm.polyglot.Value;
 
+import javax.enterprise.event.Observes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -12,18 +14,28 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
-@Path("/hello")
+@Path("/avatar")
 public class ExampleResource {
+    private Value drawMethod;
+
+    public void onStart(@Observes StartupEvent se) {
+        new Thread() {{
+        final URL drawImage = getClass().getClassLoader().getResource("hello");
+        Context polyglot = Context.newBuilder().allowAllAccess(true).build();
+        Source source = null;
+        try {
+            source = Source.newBuilder("llvm", drawImage).build();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        drawMethod = polyglot.eval(source);
+        }}.start();
+    }
 
     @GET
     @Produces(MediaType.APPLICATION_OCTET_STREAM)
-    public File hello() throws IOException {
-        final URL drawImage = getClass().getClassLoader().getResource("hello");
-        Context polyglot = Context.newBuilder().
-                allowAllAccess(true).build();
-        Source source = Source.newBuilder("llvm", drawImage).build();
-        Value cpart = polyglot.eval(source);
-        cpart.execute();
-        return new File("fruit.png");
+    public File uniqueImage() throws IOException {
+        drawMethod.execute();
+        return new File("avatar.png");
     }
 }
